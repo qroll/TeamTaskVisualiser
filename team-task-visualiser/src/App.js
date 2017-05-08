2ecca03040aa675b139605ad8ff89902badb33d7
roll <quek.ruoling@gmail.com>
2017-05-08 16:26:22 +0800

    Allow user to edit task by clicking on the task item

--- team-task-visualiser/src/App.js
+++ team-task-visualiser/src/App.js
@@ -28,6 +28,7 @@ class TaskComponent extends Component {
       ]
     }
     this.addTask = this.addTask.bind(this);
+    this.editTask = this.editTask.bind(this);
   }
 
   addTask(task) {
@@ -39,12 +40,23 @@ class TaskComponent extends Component {
     });
   }
 
+  editTask(taskId, task) {
+    this.setState({
+      tasks: [
+        ...this.state.tasks.slice(0, taskId),
+        task,
+        ...this.state.tasks.slice(taskId + 1)
+      ]
+    });
+  }
+
   render() {
     return (
       <div>
         <Form
           addTask={this.addTask} />
         <TaskList
+          editTask={this.editTask}
           tasks={this.state.tasks}  />
       </div>
     )
@@ -95,11 +107,65 @@ class TaskList extends Component {
     <div>
       <ul>
         {this.props.tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <Task key={index} 
+            editTask={this.props.editTask} 
+            taskId={index} 
+            task={task} />
         ))}
       </ul>
-    </div>)
+    </div>
+  )}
+}
+
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      value: this.props.task
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleClick(event) {
+    this.setState({
+      isEditing: true
+    })
+    event.preventDefault();
+  }
+
+  handleSubmit(event) {
+    this.props.editTask(this.props.taskId, this.state.value);
+    this.setState({
+      isEditing: false
+    });
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  render() {
+    if (this.state.isEditing) {
+      return <li>
+        <form onSubmit={this.handleSubmit} >
+          <input
+            type="text"
+            autoFocus
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </form>
+      </li>;
+    }
+    return <li onClick={this.handleClick}>{this.props.task}</li>;
+  }
+
 }
 
 export default App;
