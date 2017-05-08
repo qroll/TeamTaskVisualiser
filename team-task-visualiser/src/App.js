c86530fcf00685a0d2064278c7ed4d02deba3f45
roll <quek.ruoling@gmail.com>
2017-05-08 15:27:29 +0800

    Display and allow user to add new tasks

--- team-task-visualiser/src/App.js
+++ team-task-visualiser/src/App.js
@@ -13,8 +13,94 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TaskComponent />
       </div>
     );
+  }
+}
+
+class TaskComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        "one", "two", "three"
+      ]
+    }
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask(task) {
+    let newState = {
+      ...this.state,
+      tasks: [
+        ...this.state.tasks,
+        task
+      ]
+    };
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div>
+        <Form
+          addTask={this.addTask} />
+        <TaskList
+          tasks={this.state.tasks}  />
+      </div>
+    )
+  }
+}
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    this.props.addTask(this.state.value);
+    this.setState({
+      value: ''
+    })
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form
+          onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
+
+class TaskList extends Component {
+  render() {
+    return (
+    <div>
+      <ul>
+        {this.props.tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>)
   }
 }
 
